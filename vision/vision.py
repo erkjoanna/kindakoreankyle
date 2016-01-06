9412fc26e28ed221a30d5a0ffc7bd38c22755fb1
Jennifer Zhang <jenniferjuliazhang@gmail.com>
2016-01-06 15:14:28 -0500

    Red color detection

--- vision/vision.py
+++ vision/vision.py
@@ -2,7 +2,28 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('road.png', 0)
-plt.imshow(img, cmap='gray', interpolation='bicubic')
-plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-plt.show()
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # define range of red color in HSV
+    lower_red = np.array([17, 15, 100], dtype=np.uint8)
+    upper_red = np.array([50, 56, 200], dtype=np.uint8)
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(frame, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
